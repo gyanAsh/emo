53ac96022df1cf81c6e39a1942ddaab12454110f
gyanEkka <gyan.ekka@mathongo.com>
2023-05-05 20:26:49 +0530

    test: fixing env issue

--- src/env.mjs
+++ src/env.mjs
@@ -25,7 +25,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.POSTGRES_PRISMA_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
